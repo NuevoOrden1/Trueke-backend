a5c201fc348a02b7488d5d125d82df9acd70e2f7
NuevoOrden1 <alan.uni739@gmail.com>
2025-07-15 18:56:43 -0500

    correcion de problemas al cambiar de nombre el directorio backend

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for trueke_backend project.
+URL configuration for the Trueke project backend..
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
